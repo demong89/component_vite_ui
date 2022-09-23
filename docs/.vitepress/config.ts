fdc169369a1571103b2c0526fbd31a2910473577
guoxingwang <wb-gxw02154864@alibaba-inc.com>
2022-09-23 14:40:34 +0800

    feat: 添加文档 vitepress

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,31 @@
+const sidebar = {
+  "/": [
+    { text: "快速开始", link: "/" },
+    {
+      text: "通用",
+      children: [{ text: "Button 按钮", link: "/components/button/" }],
+    },
+    { text: "导航" },
+    { text: "反馈" },
+    { text: "数据录入" },
+    { text: "数据展示" },
+    { text: "布局" },
+  ],
+};
+const config = {
+  title: "vueUI",
+  description: "组件库搭建",
+  themeConfig: {
+    sidebar,
+  },
+
+  markdown: {
+    config: (md) => {
+      // 添加DemoBlock插槽
+      const { demoBlockPlugin } = require('vitepress-theme-demoblock')
+      md.use(demoBlockPlugin);
+    },
+  },
+};
+
+export default config;
